2587895e9e408cb0551b29bf759762cc2b30ef42
Savana Chou <sachou@chapman.edu>
2025-09-23 15:46:23 -0700

    another test

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,7 +5,7 @@ public class UIManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("Start");
+        
     }
 
     // Update is called once per frame
